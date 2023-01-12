7270432dc8ff1a9414d5599041e7b2d02a1bae57
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2023-01-13 01:35:36 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#pragma warning (disable:4996)
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int a, b;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+}
