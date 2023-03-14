d9aaf23805a1d71983717bc85844b6722ed02e9c
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2023-03-15 01:39:41 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -1,31 +1,28 @@
 #pragma warning (disable:4996)
 
 #include <iostream>
-#include <math.h>
-#include <vector>
 
 #define LENGTH 10
+#define SIZE 101
 
 using namespace std;
-
-vector<pair<int, int>> paper;
 
 int main() {
     int n;
     int sum = 0;
-    int area[101][101] = { 0, };
+    int area[SIZE][SIZE] = { 0, };
     cin >> n;
     for (int i = 0; i < n; i++) {
         int x, y;
         cin >> x >> y;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < LENGTH; i++) {
+            for (int j = 0; j < LENGTH; j++) {
                 area[x + i][y + j]++;
             }
         }
     }
-    for (int i = 0; i < 101; i++) {
-        for (int j = 0; j < 101; j++) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             if (area[i][j] > 0) sum += 1;
         }
     }
