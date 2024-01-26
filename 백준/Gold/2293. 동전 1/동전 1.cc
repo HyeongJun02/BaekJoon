96c7ac1d14990d38192baebb61efa60065b9d45e
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2024-01-26 20:14:55 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 1268 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -10,8 +10,7 @@ int dp[10001];
 int main() {
 	int n, k;
 	scanf("%d %d", &n, &k);
-	v.push_back(0);
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		int tmp;
 		scanf("%d", &tmp);
 		v.push_back(tmp);
@@ -19,12 +18,10 @@ int main() {
 
 	dp[0] = 1;
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		for (int j = v[i]; j <= k; j++) {
-			//printf("%d %d\n", i, j);
 			dp[j] += dp[j - v[i]];
 		}
 	}
-	//printf("dp[%d]: ", k);
 	printf("%d", dp[k]);
 }
