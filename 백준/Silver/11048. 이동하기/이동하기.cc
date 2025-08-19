53861910d3e299a726e578b46c674818729634ee
HyeongJun, Kim <79651429+HyeongJun02@users.noreply.github.com>
2025-08-19 09:27:35 +0900

    [Silver II] Title: 이동하기, Time: 64 ms, Memory: 9848 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+
+#include <vector>
+//#include <map>
+//#include <unordered_map>
+//#include <set>
+//#include <unordered_set>
+#include <queue>
+//#include <stack>
+
+//#include <limits>
+
+#define endl "\n"
+#define INF 2147483647
+
+typedef long long ll;
+using namespace std;
+
+int N, M;
+int room[1001][1001];
+int dp[1001][1001];
+//int maxCandy = 0;
+
+void solve() {
+}
+
+void input() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> room[i][j];
+			dp[i][j] = room[i][j];
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			dp[i][j] = room[i][j] + max(dp[i - 1][j - 1], max(dp[i - 1][j], dp[i][j - 1]));
+		}
+	}
+
+	cout << dp[N][M] << endl;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	input();
+	solve();
+}
